194cac8dbc38044093097ce870e2a1ae1436f0a0
Matías M. Méndez <matimendez88@gmail.com>
2014-04-05 15:25:12 -0300

    site structure

--- sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
@@ -1,11 +1,32 @@
-<div class="grid-100 grid-parent">
-  <?php print render($page['header']); ?>
-</div>
-<div class="grid-container">
-  <div class="grid-100 grid-parent">
-    <?php print render($page['main']); ?>
-  </div>
-</div>
-<div class="grid-100 grid-parent">
-  <?php print render($page['footer']); ?>
-</div>
+<header>
+	<div class="grid-container grid-parent">
+		<div class="logo grid-20 mobile-grid-100">
+			<?php print render($page['header_logo']); ?>
+		</div>
+		<div class="grid-80 mobile-grid-100">
+			<div class="grid-100 grid-parent">
+				<?php print render($page['header_search']); ?>
+			</div>
+			<div class="grid-100 grid-parent">
+				<?php print render($page['header_menu']); ?>
+			</div>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="grid-container grid-parent">
+		<?php print render($page['main']); ?>
+	</div>
+</main>
+<footer>
+	<div class="grid-100">
+		<div class="grid-container">
+			<?php print render($page['footer_menu']); ?>
+		</div>
+	</div>
+	<div class="grid-100">
+		<div class="grid-container">
+			<?php print render($page['footer_copyright']); ?>
+		</div>
+	</div>
+</footer>
