8e10e62b6eddad4aa5b357fc926419cbb4f3062c
Nicolas Soreian <nicosore@gmail.com>
2014-04-05 14:25:56 -0300

    Changes

--- sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
@@ -1,8 +1,11 @@
 <div class="grid-100 grid-parent">
-  <?php print render($page['full']); ?>
+  <?php print render($page['header']); ?>
 </div>
 <div class="grid-container">
   <div class="grid-100 grid-parent">
-    <?php print render($page['fixed']); ?>
+    <?php print render($page['main']); ?>
   </div>
+</div>
+<div class="grid-100 grid-parent">
+  <?php print render($page['footer']); ?>
 </div>
