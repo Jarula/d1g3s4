c0ead092faeb0161ddd3bd8a51cb7ac12af01f83
Nicolas Soreian <nicosore@gmail.com>
2014-04-05 12:31:07 -0300

    Layout Base Site

---
+++ sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
@@ -0,0 +1,8 @@
+<div class="grid-100 grid-parent">
+  <?php print render($page['full']); ?>
+</div>
+<div class="grid-container">
+  <div class="grid-100 grid-parent">
+    <?php print render($page['fixed']); ?>
+  </div>
+</div>
