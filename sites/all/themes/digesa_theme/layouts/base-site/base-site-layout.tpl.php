cc47d2a52195425b613151bc87a7cacac2e638c1
Matías M. Méndez <matimendez88@gmail.com>
2014-04-08 01:02:07 -0300

    styling header and footer

--- sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/base-site/base-site-layout.tpl.php
@@ -1,13 +1,13 @@
 <header>
 	<div class="grid-container grid-parent">
-		<div class="logo grid-20 mobile-grid-100">
+		<div class="grid-20 mobile-grid-100 logo">
 			<?php print render($page['header_logo']); ?>
 		</div>
 		<div class="grid-80 mobile-grid-100">
-			<div class="grid-100 grid-parent">
+			<div class="grid-100 grid-parent search-bar">
 				<?php print render($page['header_search']); ?>
 			</div>
-			<nav id="main-menu" class="grid-100 grid-parent">
+			<nav id="main-menu" class="grid-100 grid-parent menu">
 				<?php print render($page['header_menu']); ?>
 			</nav>
 		</div>
@@ -19,12 +19,12 @@
 	</div>
 </main>
 <footer>
-	<div class="grid-100">
+	<div class="grid-100 footer-menu">
 		<div class="grid-container">
 			<?php print render($page['footer_menu']); ?>
 		</div>
 	</div>
-	<div class="grid-100">
+	<div class="grid-100 footer-copyright">
 		<div class="grid-container">
 			<?php print render($page['footer_copyright']); ?>
 		</div>
