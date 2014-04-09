24df9c665a76f9cce2aef0db633baf898d25d456
Nicolas Soreian <nicosore@gmail.com>
2014-04-08 22:48:12 -0300

    Layout One Top and Three Column

---
+++ sites/all/themes/digesa_theme/layouts/top-three-column/top-three-columns-layout.tpl.php
@@ -0,0 +1,14 @@
+<div class="grid-100">
+	<?php print render($page['top']); ?>
+</div>
+<div>
+	<div class="grid-33 grid-mobile-100">
+		<?php print render($page['left']); ?>
+	</div>
+	<div class="grid-33 grid-mobile-100">
+        	<?php print render($page['center']); ?>
+	</div>
+	<div class="grid-33 hide-on-mobile">
+        	<?php print render($page['right']); ?>
+	</div>
+</div>
