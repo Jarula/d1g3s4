6b9d057eb29e0d891993eed7e2c49487dbaa3222
Nicolas Soreian <nicolas@varcelona.com>
2014-05-08 13:51:47 -0300

    Nueva version del Widget Map

--- sites/all/modules/custom/jarula_widgets/js/widget_map/map.js
+++ sites/all/modules/custom/jarula_widgets/js/widget_map/map.js
@@ -1,58 +1,4 @@
-/*function initialize() {
-  var latitude = Drupal.settings.key[0]['latitude'];
-  var longitude = Drupal.settings.key[0]['longitude'];
-  var array_zoom = parseInt(Drupal.settings.key[0]['zoom']);
-  var maptype = Drupal.settings.key[0]['maptype'];
-
-  var myLatlng = new google.maps.LatLng(latitude, longitude);
-
-  var mapOptions = {
-    zoom: array_zoom,
-    center: myLatlng,
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  // Add 5 markers to the map at random locations
-  var southWest = new google.maps.LatLng(-31.203405, 125.244141);
-  var northEast = new google.maps.LatLng(-25.363882, 131.044922);
-
-  var bounds = new google.maps.LatLngBounds(southWest, northEast);
-  map.fitBounds(bounds);
-
-  var lngSpan = northEast.lng() - southWest.lng();
-  var latSpan = northEast.lat() - southWest.lat();
-
-  for (var i = 0; i < 5; i++) {
-    var position = new google.maps.LatLng(
-        southWest.lat() + latSpan * Math.random(),
-        southWest.lng() + lngSpan * Math.random());
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map
-    });
-
-    marker.setTitle((i + 1).toString());
-    attachSecretMessage(marker, i);
-  }
-}
-
-// The five markers show a secret message when clicked
-// but that message is not within the marker's instance data
-function attachSecretMessage(marker, num) {
-  var message = ['This', 'is', 'the', 'secret', 'message'];
-  var infowindow = new google.maps.InfoWindow({
-    content: message[num]
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(marker.get('map'), marker);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-*/
+/*
 function initialize() {
 
   var latitude = Drupal.settings.key[0]['latitude'];
@@ -74,4 +20,53 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+*/
 
+//var latitude = Drupal.settings.key[0]['latitude'];
+//var longitude = Drupal.settings.key[0]['longitude'];
+//var maptype = Drupal.settings.key[0]['maptype'];
+
+function initialize() {
+  var markers = Drupal.settings.key['markers'];
+  var array_zoom = parseInt(Drupal.settings.key['zoom']);
+
+  var mapOptions = {
+    zoom: array_zoom,
+    center: new google.maps.LatLng(-34.60227, -58.386154),
+  };
+
+  var bounds = new google.maps.LatLngBounds();
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  for (var i = 0; i < markers.length; i++) {
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    var infoWindowContent = markers[i][0];
+    var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+    bounds.extend(position);
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: infoWindowContent,
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            infoWindow.setContent(infoWindowContent);
+            infoWindow.open(map, marker);
+        }
+    })(marker, i));
+
+  }
+
+  map.fitBounds(bounds);
+  
+  var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+      this.setZoom(14);
+      google.maps.event.removeListener(boundsListener);
+  });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
