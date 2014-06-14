40ca0d3e2d1577ac7c2da7717864861c75233f28
Matías M. Méndez <matimendez88@gmail.com>
2014-06-14 17:39:52 -0300

    styles

--- sites/all/modules/custom/jarula_widgets/templates/widget-link.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-link.tpl.php
@@ -1,7 +1,6 @@
 <?php
 foreach($widget_link as $link){
 	print '<div class="'.$class.' grid-mobile-100 link-item">';
-	print ' <div class="filler"></div> ';
         (isset($link['link'])) ? print $link['link'] : '';
         (isset($link['title'])) ? print $link['title'] : '';
 	echo '</div>';
