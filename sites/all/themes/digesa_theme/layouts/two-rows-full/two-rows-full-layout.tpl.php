5a64c1abaf6622fc4bd2569b0034fad561d5dc97
Nicolas Soreian <nicosore@gmail.com>
2014-06-16 20:19:29 -0300

    Cambio de ancho del sitio a 960

--- sites/all/themes/digesa_theme/layouts/two-rows-full/two-rows-full-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/two-rows-full/two-rows-full-layout.tpl.php
@@ -1,10 +1,14 @@
 <div class="grid-container grid-parent clearfix">
-	<div class="grid-100 grid-parent">
-		<?php print render($page['top']); ?>
+	<div class="grid-80 prefix-10 suffix-10 grid-parent">
+		<div class="grid-100">
+			<?php print render($page['top']); ?>
+		</div>
 	</div>
 </div>
 <div class="full-width clearfix">
 	<div class="grid-container grid-parent">
-		<?php print render($page['bottom']); ?>
+		<div class="grid-80 prefix-10 suffix-10 grid-parent">
+			<?php print render($page['bottom']); ?>
+		</div>
 	</div>
 </div>
--- sites/all/themes/digesa_theme/layouts/two-rows/two-rows-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/two-rows/two-rows-layout.tpl.php
@@ -1,8 +1,10 @@
 <div class="grid-container grid-parent">
-	<div class="grid-100 grid-parent">
-		<?php print render($page['top']); ?>
-	</div>
-	<div class="grid-100 grid-parent">
-		<?php print render($page['bottom']); ?>
+	<div class="grid-80 prefix-10 suffix-10 grid-parent">
+		<div class="grid-100 grid-parent">
+			<?php print render($page['top']); ?>
+		</div>
+		<div class="grid-100 grid-parent">
+			<?php print render($page['bottom']); ?>
+		</div>
 	</div>
 </div>
