6c0a5107f44abd947815a99df1ce859a29729167
Nicolas Soreian <nicosore@gmail.com>
2014-06-23 22:34:15 -0300

    Ajustes en el Mapa

--- sites/all/modules/custom/jarula_widgets/js/widget_map/map.js
+++ sites/all/modules/custom/jarula_widgets/js/widget_map/map.js
@@ -1,34 +1,7 @@
-/*
-function initialize() {
-
-  var latitude = Drupal.settings.key[0]['latitude'];
-  var longitude = Drupal.settings.key[0]['longitude'];
-  var array_zoom = parseInt(Drupal.settings.key[0]['zoom']);
-  var maptype = Drupal.settings.key[0]['maptype'];
-  
-  var myLatlng = new google.maps.LatLng(latitude, longitude);
-  var mapOptions = {
-    zoom: array_zoom,
-    center: myLatlng,
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-*/
-
-//var latitude = Drupal.settings.key[0]['latitude'];
-//var longitude = Drupal.settings.key[0]['longitude'];
-//var maptype = Drupal.settings.key[0]['maptype'];
-
 function initialize() {
   var markers = Drupal.settings.key['markers'];
   var array_zoom = parseInt(Drupal.settings.key['zoom']);
+  var markerClusterer = null;
 
   var mapOptions = {
     zoom: array_zoom,
@@ -44,10 +17,14 @@ function initialize() {
     var infoWindow = new google.maps.InfoWindow(), marker, i;
     var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
     bounds.extend(position);
+
     var marker = new google.maps.Marker({
       position: position,
+      animation: google.maps.Animation.DROP,
       map: map,
+      icon: Drupal.settings.basePath+Drupal.settings.icon,
     });
+
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
             infoWindow.setContent(markers[i][0]);
@@ -58,12 +35,14 @@ function initialize() {
   map.fitBounds(bounds);
 
   }
-  
+
   var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-      this.setZoom(14);
+      var theZoom = map.getZoom();
+      this.setZoom(theZoom > 14 ? 14 : theZoom);
       google.maps.event.removeListener(boundsListener);
   });
 
 }
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
