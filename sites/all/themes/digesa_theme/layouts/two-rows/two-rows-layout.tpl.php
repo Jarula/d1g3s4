41d6ec1ebb5352964ee2efa22dc59fe31be0d723
Matías M. Méndez <matimendez88@gmail.com>
2014-07-12 12:19:29 -0300

    estilos página de contacto

--- sites/all/themes/digesa_theme/layouts/two-rows/two-rows-layout.tpl.php
+++ sites/all/themes/digesa_theme/layouts/two-rows/two-rows-layout.tpl.php
@@ -1,6 +1,6 @@
 <div class="grid-container grid-parent">
 	<div class="grid-80 prefix-10 suffix-10 grid-parent">
-		<div class="grid-100 grid-parent">
+		<div class="grid-100">
 			<?php print render($page['top']); ?>
 		</div>
 		<div class="grid-100 grid-parent">
