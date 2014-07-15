dad264683cab9de7de7ad9aa7767c91fd01a6395
Nicolas Soreian <nicosore@gmail.com>
2014-07-14 23:50:19 -0300

    Search

---
+++ sites/all/modules/custom/jarula_widgets/templates/widget-search.tpl.php
@@ -0,0 +1,19 @@
+<?php
+foreach(array_chunk($widget_search,3,true) as $items){
+	foreach($items as $item){
+?>
+<article class="grid-33">
+<a href="<?php print $item['destination']; ?>" title="<?php print $item['title']; ?>">
+    <?php print $item['image']; ?>
+    <h1><?php print $item['title']; ?></h1>
+    <p><?php print $item['street']; ?></p>
+    <?php print $item['status']; ?>
+  </a>
+</article>
+<?php
+}
+?>
+<div class="clearfix"></div>
+<?php
+}
+?>
