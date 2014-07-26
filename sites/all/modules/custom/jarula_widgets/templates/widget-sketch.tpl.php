23882f61371b63977bca5e18070d1c3120542760
Nicolas Soreian <nicosore@gmail.com>
2014-07-26 12:26:11 -0300

    Wrapper para Sketch

--- sites/all/modules/custom/jarula_widgets/templates/widget-sketch.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-sketch.tpl.php
@@ -1,10 +1,12 @@
-<div class="grid-25"><span>Planos</span></div>
-<div class="grid-75">
-  <div class="container" style="display:none;">
-  <?php
-  foreach($images as $image){
-    print $image;
-  }
-  ?>
+<div class="widget-sketch-wrapper">
+  <div class="grid-25"><span>Planos</span></div>
+  <div class="grid-75">
+    <div class="container" style="display:none;">
+    <?php
+    foreach($images as $image){
+      print $image;
+    }
+    ?>
+    </div>
   </div>
 </div>
