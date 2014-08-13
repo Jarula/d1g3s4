8c9e5f43e4e070ca413ff8f74f02b6a2ed9cfab2
Nicolas Soreian <nicosore@gmail.com>
2014-08-13 20:49:36 -0300

    Funcionalidad de Slider en el home para Widgets Destacados

--- sites/all/modules/custom/jarula_widgets/templates/widget-custom-card-list.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-custom-card-list.tpl.php
@@ -1,10 +1,33 @@
-<?php foreach($widget_custom_card_list as $item){ ?>
-<article class="grid-33">
-<a href="<?php print $item['destination']; ?>" title="<?php print $item['title']; ?>">
-    <?php print $item['image']; ?>
-    <h1><?php print $item['title']; ?></h1>
-    <p><?php print $item['street']; ?></p>
-    <?php print $item['status']; ?>
-  </a>
-</article>
-<?php } ?>
+<div class="flexslider">
+  <ul class="slides">
+    <?php foreach($widget_custom_card_list as $item){ ?>
+    <li class="grid-33">
+      <a href="<?php print $item['destination']; ?>" title="<?php print $item['title']; ?>">
+        <?php print $item['image']; ?>
+        <h1><?php print $item['title']; ?></h1>
+        <p><?php print $item['street']; ?></p>
+        <?php print $item['status']; ?>
+      </a>
+    </li>
+    <?php } ?>
+  </ul>
+</div>
+
+<script type="text/javascript">
+jQuery(window).load(function() {
+  jQuery('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 315,
+    itemMargin: 3,
+    controlNav: false,
+    smoothHeight: true,
+    randomize: true,
+    prevText: 'Anterior',
+    nextText: 'Siguiente',
+    start: function(slider) {
+      jQuery( "body" ).removeClass('loading');
+    }
+  });
+});
+</script>
