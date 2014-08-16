6e014be54396c92370856a09be9156de9ad5d405
Nicolas Soreian <nicosore@gmail.com>
2014-08-16 14:20:18 -0300

    Ajustes en los layouts

--- sites/all/modules/custom/jarula_widgets/templates/widget-slider-home.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-slider-home.tpl.php
@@ -1,11 +1,10 @@
 <div class="fluid_container">
   <div class="camera_wrap camera_emboss pattern_1" id="background-slider">
     <?php foreach($widget_slider as $item) { ?>
-    <div data-thumb="<?php $item['image']; ?>" data-src="<?php print $item['image']; ?>"></div>
+    <div data-thumb="<?php print $item['image']; ?>" data-src="<?php print $item['image']; ?>"></div>
     <?php } ?>
   </div>
 </div>
-
 
 <script type="text/javascript">
   jQuery(function(){
