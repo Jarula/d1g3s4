015647d7e2a71620daf7a2adb4551f18c1f9a0c4
Nicolas Soreian <nicosore@gmail.com>
2014-08-16 18:46:44 -0300

    Layout para la edicion/creacion de fichas

---
+++ sites/all/themes/digesa_theme/layouts/node-edit/node-edit-layout.tpl.php
@@ -0,0 +1,15 @@
+<div class="grid-container">
+  <div class="grid-80 prefix-10 suffix-10 grid-parent">
+    <div class="grid-70">
+      <div class="grid-100 custom-form-wrapper grid-parent">
+        <?php print render($page['street']); ?>
+      </div>
+      <div class="grid-100 custom-form-wrapper grid-parent">
+        <?php print render($page['slider']); ?>
+      </div>
+    </div>
+    <div class="grid-30">
+      <?php print render($page['sidebar']); ?>
+    </div>
+  </div>
+</div>
