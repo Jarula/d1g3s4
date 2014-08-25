b0759f7040e589c8224e81d3605d7197df5a8cf0
Matías M. Méndez <matimendez88@gmail.com>
2014-08-25 03:58:56 -0300

    miniaturas home

--- sites/all/modules/custom/jarula_widgets/templates/widget-link.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-link.tpl.php
@@ -1,8 +1,11 @@
 <?php
-foreach($widget_link as $link){
-	print '<div class="'.$class.' grid-mobile-100 link-item">';
-        (isset($link['link'])) ? print $link['link'] : '';
-        (isset($link['title'])) ? print $link['title'] : '';
-	echo '</div>';
-}
+echo '<div class="link-item-container">';
+	foreach($widget_link as $link){
+		print '<div class="'.$class.' grid-mobile-100 link-item">';
+	        (isset($link['link'])) ? print $link['link'] : '';
+	        (isset($link['title'])) ? print $link['title'] : '';
+		echo '</div>';
+	}
+echo '</div>';
+echo '<div class="link-item-background"></div>';
 ?>
