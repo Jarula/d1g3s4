5bad210bfce107e03a196a7d779f952ef9e31485
Matías M. Méndez <matimendez88@gmail.com>
2014-08-30 14:23:32 -0300

    miniaturas home

--- sites/all/modules/custom/jarula_widgets/templates/widget-custom-card-list.tpl.php
+++ sites/all/modules/custom/jarula_widgets/templates/widget-custom-card-list.tpl.php
@@ -1,33 +1,39 @@
 <div class="flexslider">
-  <ul class="slides">
-    <?php foreach($widget_custom_card_list as $item){ ?>
-    <li class="grid-33">
-      <a href="<?php print $item['destination']; ?>" title="<?php print $item['title']; ?>">
-        <?php print $item['image']; ?>
-        <h1><?php print $item['title']; ?></h1>
-        <p><?php print $item['street']; ?></p>
-        <?php print $item['status']; ?>
-      </a>
-    </li>
-    <?php } ?>
-  </ul>
+    <ul class="slides link-item-container">
+        <?php foreach($widget_custom_card_list as $item){ ?>
+            <li class="grid-33 link-item">
+                <a href="<?php print $item['destination']; ?>" title="<?php print $item['title']; ?>">
+                    <?php print $item['image']; ?>
+                    <div class="card-list-info">
+                        <h1><?php print $item['title']; ?></h1>
+                        <p><?php print $item['street']; ?></p>
+                    </div>
+                    <?php print $item['status']; ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+    <div class="link-item-background"></div>
 </div>
 
 <script type="text/javascript">
-jQuery(window).load(function() {
-  jQuery('.flexslider').flexslider({
-    animation: "slide",
-    animationLoop: true,
-    itemWidth: 315,
-    itemMargin: 3,
-    controlNav: false,
-    smoothHeight: true,
-    randomize: true,
-    prevText: 'Anterior',
-    nextText: 'Siguiente',
-    start: function(slider) {
-      jQuery( "body" ).removeClass('loading');
-    }
-  });
-});
+    jQuery(window).load(function() {
+        jQuery('.flexslider').flexslider({
+            animation: "slide",
+            animationLoop: true,
+            controlNav: true,
+            direction: "horizontal",
+            itemWidth: 315,
+            itemMargin: 3,
+            minItems: 5,
+            maxItems: 5,
+            nextText: '',
+            prevText: '',
+            randomize: true,
+            smoothHeight: true,
+            start: function(slider) {
+                jQuery( "body" ).removeClass('loading');
+            }
+        });
+    });
 </script>
